b57c3f284dfa98d87c8f8463e2209cdee0d984d4
Kyle Geske <stungeye@gmail.com>
2016-02-04 14:52:00 -0500

    Renamed and repositioned the feed fixture path constants.

--- spec/disposition_feed_spec.rb
+++ spec/disposition_feed_spec.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 require 'spec_helper'
 
-FEED_FIXTURE = File.dirname(__FILE__) +
-               '/fixtures/disposition_feed.json'.freeze
-MALFORMED_FEED_FIXTURE = File.dirname(__FILE__) +
-                         '/fixtures/disposition_feed_malformed.json'.freeze
-
 describe DispositionFeed do
+  FEED_PATH = File.dirname(__FILE__) +
+              '/fixtures/disposition_feed.json'.freeze
+
+  MALFORMED_FEED_PATH = File.dirname(__FILE__) +
+                        '/fixtures/disposition_feed_malformed.json'.freeze
+
   context 'with a well-formed Disposition Feed JSON file' do
-    subject(:disposition_feed) { DispositionFeed.new(FEED_FIXTURE) }
+    subject(:disposition_feed) { DispositionFeed.new(FEED_PATH) }
 
     it 'should be the correct type' do
       expect(disposition_feed.class).to eq(DispositionFeed)
@@ -18,7 +19,7 @@ describe DispositionFeed do
   context 'with a malformed Disposition Feed JSON file' do
     it 'should raise a JSON Parser error' do
       expect do
-        DispositionFeed.new(MALFORMED_FEED_FIXTURE)
+        DispositionFeed.new(MALFORMED_FEED_PATH)
       end.to raise_error(JSON::ParserError)
     end
   end
