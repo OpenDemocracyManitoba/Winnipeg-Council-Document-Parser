068202becb019cf202a7c4c9e806d351c644dd8e
Kyle Geske <stungeye@gmail.com>
2016-02-11 16:06:00 -0500

    Ignoring Rubocop line length cop wihtin a disposition feed spec.

--- spec/disposition_feed_spec.rb
+++ spec/disposition_feed_spec.rb
@@ -24,14 +24,16 @@ describe DispositionFeed do
     end
 
     it 'should be able to provide access to meeting data by name' do
+      # rubocop:disable Metrics/LineLength
       meeting = disposition_feed.regular_meetings.first
-      expect(meeting[:row]).to eq(1)
-      expect(meeting[:id]).to eq('4E4B0D84-3493-40CC-8585-578C7A118A34')
+      expect(meeting[:row]).to          eq(1)
+      expect(meeting[:id]).to           eq('4E4B0D84-3493-40CC-8585-578C7A118A34')
       expect(meeting[:meeting_date]).to eq(Date.new(2015, 9, 30))
-      expect(meeting[:type]).to eq('Regular Meeting')
-      expect(meeting[:url]).to eq('https://data.winnipeg.ca/download/jfmn-ak46/application/msword')
+      expect(meeting[:type]).to         eq('Regular Meeting')
+      expect(meeting[:url]).to          eq('https://data.winnipeg.ca/download/jfmn-ak46/application/msword')
       expect(meeting[:publish_date]).to eq(Date.new(2015, 9, 30))
-      expect(meeting[:update_date]).to eq(Date.new(2015, 9, 30))
+      expect(meeting[:update_date]).to  eq(Date.new(2015, 9, 30))
+      # rubocop:enable Metrics/LineLength
     end
   end
 
