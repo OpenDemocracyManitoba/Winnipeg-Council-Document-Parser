977f4043236a081b973e157a8d934deff8eccdf3
Kyle Geske <stungeye@gmail.com>
2016-09-17 07:48:52 -0500

    Script to convert DOCX to JSON.

---
+++ disposition_docx_to_json.rb
@@ -0,0 +1,20 @@
+# Script to convert DOCX dispositions into JSON.
+# 
+# Required command line argument: DOCX Dispoition Filename
+#
+# Generated JSON is sent to STDOUT.
+
+require 'json'
+require_relative 'disposition'
+
+if ARGV.size != 1
+  puts 'Missing required arguments.'
+  puts "Example: #{$PROGRAM_NAME} some_disposition.docx"
+  exit
+end
+
+docx_input_filepath = ARGV[0]
+json_output_filepath = ARGV[1]
+
+disposition = Disposition.new(docx_input_filepath)
+puts JSON.generate(disposition.to_h)
