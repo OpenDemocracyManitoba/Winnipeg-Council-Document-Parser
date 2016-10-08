1a1adebce3a258b91087f26c94f236c62803c3dc
Kyle Geske <stungeye@gmail.com>
2016-10-08 07:44:30 -0500

    Completed the json to html Ruby script.

--- disposition_json_to_html.rb
+++ disposition_json_to_html.rb
@@ -1,6 +1,5 @@
 require './file_helpers.rb'
 require './erb_binding.rb'
-require './hansard.rb'
 
 if ARGV.size != 2
   puts 'Missing required arguments.'
@@ -8,14 +7,10 @@ if ARGV.size != 2
   exit
 end
 
-json_hansard   = json_hansard_from_file(ARGV[0])
-erb_template   = erb_template_from_file(ARGV[1])
-stop_words     = stopwords_from_file('./stopwords.txt')
-
-hansard        = Hansard.new(json_hansard: json_hansard,
-                             stop_words:   stop_words)
+disposition  = json_disposition_from_file(ARGV[0])
+erb_template = erb_template_from_file(ARGV[1])
 
 generated_html = ErbBinding.new(erb_template: erb_template,
-                                data_to_bind: hansard)
+                                data_to_bind: disposition)
 
 puts generated_html.render
