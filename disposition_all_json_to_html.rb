e8e4f312b7e93fe771d2d22940990371d7e5210a
Kyle Geske <stungeye@gmail.com>
2016-10-09 08:49:50 -0500

    Converting all dispositions with the new convert all script. No meta data used yet.

--- disposition_all_json_to_html.rb
+++ disposition_all_json_to_html.rb
@@ -1,12 +1,15 @@
+require './file_helpers.rb'
+require './erb_binding.rb'
 
-if ARGV.size != 2
+if ARGV.size != 3
   puts 'Missing required arguments.'
-  puts 'Example: #{$PROGRAM_NAME} input_folder output_folder'
+  puts 'Example: #{$PROGRAM_NAME} erb_template input_folder output_folder'
   exit
 end
 
-input_folder = ARGV[0]
-output_folder = ARGV[1]
+erb_template = erb_template_from_file(ARGV[0])
+input_folder = ARGV[1]
+output_folder = ARGV[2]
 
 disposition_meta = {
   '2015-09-30' => { youtube: 'QXbU0ln7lo8', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=14677&SectionId=&InitUrl=' },
@@ -22,3 +25,19 @@ disposition_meta = {
   #'2016-09-28' => { youtube: 'fXQYVzhJgVY', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=15468&SectionId=&InitUrl=' },
 }
 
+def date_to_json_url(date)
+  "https://github.com/OpenDemocracyManitoba/Winnipeg-Council-Document-Parser/blob/master/json_dispositions/DISPOSITION-#{date}.json"
+end
+
+disposition_meta.each do |date, meta|
+  json_file   = "#{input_folder}/DISPOSITION-#{date}.json"
+  html_file   = "#{output_folder}/disposition-#{date}.html"
+
+  disposition = json_disposition_from_file(json_file)
+  generated_html = ErbBinding.new(erb_template: erb_template,
+                                  data_to_bind: disposition)
+
+  File.open(html_file, 'w:UTF-8') do |f|
+    f.write(generated_html.render)
+  end
+end
