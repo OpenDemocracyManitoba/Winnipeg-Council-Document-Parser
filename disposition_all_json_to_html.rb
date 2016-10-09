ca354294dd066f5eefd77add5417621379e8fc0c
Kyle Geske <stungeye@gmail.com>
2016-10-09 08:56:38 -0500

    Proper youtube embbed are being pulled from meta data when converting all json to html.

--- disposition_all_json_to_html.rb
+++ disposition_all_json_to_html.rb
@@ -12,17 +12,17 @@ input_folder = ARGV[1]
 output_folder = ARGV[2]
 
 disposition_meta = {
-  '2015-09-30' => { youtube: 'QXbU0ln7lo8', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=14677&SectionId=&InitUrl=' },
-  '2015-10-28' => { youtube: 'dZ2pS4zb7Cs', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=14759&SectionId=&InitUrl=' },
-  '2015-11-25' => { youtube: 'n3-lp7synvo', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=14855&SectionId=&InitUrl=' },
-  '2015-12-09' => { youtube: '9vuFasdioUs', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=14903&SectionId=&InitUrl=' },
-  '2016-02-24' => { youtube: 'Rd4ByL6C4pM', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=15073&SectionId=&InitUrl=' },
-  '2016-03-23' => { youtube: 'TDIRkMT2_N0', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=15162&SectionId=&InitUrl=' },
-  '2016-04-27' => { youtube: 'kj7N7cmnktg', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=15242&SectionId=&InitUrl=' },
-  '2016-05-18' => { youtube: 'fAsou9J29no', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=15306&SectionId=&InitUrl=' },
-  '2016-06-15' => { youtube: 'mfyigVDfy_s', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=15382&SectionId=&InitUrl=' },
-  '2016-07-13' => { youtube: 'OCK3nx5PQYo', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=15468&SectionId=&InitUrl=' }
-  #'2016-09-28' => { youtube: 'fXQYVzhJgVY', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=15468&SectionId=&InitUrl=' },
+  '2015-09-30' => { 'youtube' => 'QXbU0ln7lo8', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=14677&SectionId=&InitUrl=' },
+  '2015-10-28' => { 'youtube' => 'dZ2pS4zb7Cs', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=14759&SectionId=&InitUrl=' },
+  '2015-11-25' => { 'youtube' => 'n3-lp7synvo', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=14855&SectionId=&InitUrl=' },
+  '2015-12-09' => { 'youtube' => '9vuFasdioUs', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=14903&SectionId=&InitUrl=' },
+  '2016-02-24' => { 'youtube' => 'Rd4ByL6C4pM', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=15073&SectionId=&InitUrl=' },
+  '2016-03-23' => { 'youtube' => 'TDIRkMT2_N0', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=15162&SectionId=&InitUrl=' },
+  '2016-04-27' => { 'youtube' => 'kj7N7cmnktg', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=15242&SectionId=&InitUrl=' },
+  '2016-05-18' => { 'youtube' => 'fAsou9J29no', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=15306&SectionId=&InitUrl=' },
+  '2016-06-15' => { 'youtube' => 'mfyigVDfy_s', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=15382&SectionId=&InitUrl=' },
+  '2016-07-13' => { 'youtube' => 'OCK3nx5PQYo', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=15468&SectionId=&InitUrl=' }
+  #'2016-09-28' => { 'youtube' => 'fXQYVzhJgVY', dmis: 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=15468&SectionId=&InitUrl=' },
 }
 
 def date_to_json_url(date)
@@ -34,6 +34,11 @@ disposition_meta.each do |date, meta|
   html_file   = "#{output_folder}/disposition-#{date}.html"
 
   disposition = json_disposition_from_file(json_file)
+
+  # Inject Metadata
+  disposition.merge!(meta)
+  disposition.merge!({ source_json: date_to_json_url(date) })
+
   generated_html = ErbBinding.new(erb_template: erb_template,
                                   data_to_bind: disposition)
 
