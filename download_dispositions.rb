8269f90e908726528a38d685be28022558ee695f
Kyle Geske <stungeye@gmail.com>
2016-12-29 08:05:38 -0600

    Download script now includes option parsing to run in test mode or to only download latest dispositions.

--- download_dispositions.rb
+++ download_dispositions.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'open-uri'
+require 'optparse'
 require_relative 'disposition_feed'
 
 FEED_URL = 'https://data.winnipeg.ca/api/views/hsbq-sj6t/rows.json?accessType=DOWNLOAD'.freeze
@@ -13,16 +14,39 @@ def download_file(url, save_file)
   end
 end
 
+options = { testmode: false, latestonly: false, folder: '.' }
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{__FILE__} [options]"
+  opts.separator 'Downloads disposition documents from data.winnipeg.ca.'
+  opts.separator 'Options:'
+  opts.on('-t', '--testmode', 'Test only. Does not download files.') do |t|
+    options[:testmode] = t
+  end
+  opts.on('-l', '--latestonly', 'Do not overwrite existing files.') do |l|
+    options[:latestonly] = l
+  end
+  opts.on('-f', '--folder FOLDER', 'Specify a download folder.') do |f|
+    options[:folder] = f
+  end
+end.parse!
+
 download_file(FEED_URL, TEMP_FEED_FILE)
 
 parsed_disposition_feed = DispositionFeed.new(TEMP_FEED_FILE)
 regular_dispositions = parsed_disposition_feed.regular_dispositions
 
+puts 'Test Mode: No downloads' if options[:testmode]
+
 regular_dispositions.each do |disposition|
   date = disposition[:meeting_date][0..9]
-  save_file = "DISPOSITION-#{date}.docx"
-  puts "Downloading #{save_file}"
-  download_file(disposition[:url], save_file)
+  save_file = File.join(options[:folder], "DISPOSITION-#{date}.docx")
+  if options[:latestonly] && File.exist?(save_file)
+    puts "Skipping existing copy of #{save_file}"
+  else
+    puts "Downloading #{save_file}"
+    download_file(disposition[:url], save_file) unless options[:testmode]
+  end
 end
 
 File.delete(TEMP_FEED_FILE) if File.exist?(TEMP_FEED_FILE)
