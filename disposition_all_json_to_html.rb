3fd58920ade7785ec70ac90b8e45c1085bdc68bc
Kyle Geske <stungeye@gmail.com>
2018-05-13 09:12:50 -0500

    April 2018 Word Disposition parsed and html output.

--- disposition_all_json_to_html.rb
+++ disposition_all_json_to_html.rb
@@ -44,7 +44,8 @@ disposition_meta = {
   '2017-12-13' => { 'youtube' => 'pBzo3FDIg8E', 'dmis' => 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=16669&SectionId=&InitUrl=' },
   '2018-01-25' => { 'youtube' => 'evw2m-G_5XE', 'dmis' => 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=16764&SectionId=&InitUrl=' },
   '2018-02-22' => { 'youtube' => 'aT7BQhhNnLY', 'dmis' => 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=16845&SectionId=&InitUrl=' },
-  '2018-03-22' => { 'youtube' => 'bxSbEND4jSs', 'dmis' => 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=16926&SectionId=&InitUrl=' }
+  '2018-03-22' => { 'youtube' => 'bxSbEND4jSs', 'dmis' => 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=16926&SectionId=&InitUrl=' },
+  '2018-04-26' => { 'youtube' => 'BSx9VQ0Fx8Y', 'dmis' => 'http://clkapps.winnipeg.ca/dmis/ViewDoc.asp?DocId=17015&SectionId=&InitUrl=' }
 }
 
 all_dispositions = []
