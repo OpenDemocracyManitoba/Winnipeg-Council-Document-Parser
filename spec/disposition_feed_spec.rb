9b0b70103c2ebe69c3c8c51376a13b8ff89285d5
Kyle Geske <stungeye@gmail.com>
2020-03-07 14:07:50 +0000

    Updated fixtures and spec to reflect updated real world json feed data.

--- spec/disposition_feed_spec.rb
+++ spec/disposition_feed_spec.rb
@@ -16,23 +16,19 @@ describe DispositionFeed do
     end
 
     it 'should identify the correct number of regular dispositions' do
-      expect(disposition_feed.regular_dispositions.size).to eq(5)
+      expect(disposition_feed.regular_dispositions.size).to eq(47)
     end
 
     it 'should identify the correct number of special dispositions' do
-      expect(disposition_feed.special_dispositions.size).to eq(2)
+      expect(disposition_feed.special_dispositions.size).to eq(9)
     end
 
     it 'should be able to provide access to meeting data by name' do
       # rubocop:disable Metrics/LineLength
       meeting = disposition_feed.regular_dispositions.first
-      expect(meeting[:row]).to          eq(1)
-      expect(meeting[:id]).to           eq('4E4B0D84-3493-40CC-8585-578C7A118A34')
-      expect(meeting[:meeting_date]).to eq('2015-09-30T00:00:00')
+      expect(meeting[:meeting_date]).to eq('2020-01-30T00:00:00.000')
       expect(meeting[:type]).to         eq('Regular Meeting')
-      expect(meeting[:url]).to          eq('https://data.winnipeg.ca/download/jfmn-ak46/application/msword')
-      expect(meeting[:publish_date]).to eq('2015-09-30T00:00:00')
-      expect(meeting[:update_date]).to  eq('2015-09-30T00:00:00')
+      expect(meeting[:url]).to          eq('https://data.winnipeg.ca/download/kc4f-2bge/application%2Fvnd.openxmlformats-officedocument.wordprocessingml.document')
       # rubocop:enable Metrics/LineLength
     end
   end
